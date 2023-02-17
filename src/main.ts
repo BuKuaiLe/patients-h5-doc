aa2a017d1a12180db97d85718506cf04e5387386
xiongdong <1911417818@qq.com>
2023-02-17 10:34:38 +0800

    配置vw

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.css'
-
+// 1. 引入你需要的组件
+import { Button } from 'vant'
+// 2. 引入组件样式
+import 'vant/lib/index.css'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Button)
 
 app.mount('#app')
